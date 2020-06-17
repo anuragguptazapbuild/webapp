b4c1c2a306fd604b95b6e6117016a247a8afbfe6
Anurag Gupta <anuraggupta.zapbuild@gmail.com>
2020-06-17 12:03:28 +0530

    Updates

---
+++ precache-manifest.aa8d517e516855f426347bed291ff705.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "afa0947f4e53a29f7f4c",
+    "url": "/static/js/app.216d5051.chunk.js"
+  },
+  {
+    "revision": "3636aebd34579cc662c5",
+    "url": "/static/js/runtime~app.4b0f5a04.js"
+  },
+  {
+    "revision": "eacca86d2ff10f1e5abc",
+    "url": "/static/js/2.d4c7d79c.chunk.js"
+  },
+  {
+    "revision": "df234fc0669a01d8937b98568fe3a637",
+    "url": "/static/media/forest.df234fc0.jpg"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/expo-service-worker.js"
+  },
+  {
+    "revision": "afd5733ac789ab1795f1ed7a26e1ecd3",
+    "url": "/favicon-16.png"
+  },
+  {
+    "revision": "3d85359b37ca7590f8e91d593d02ddaf",
+    "url": "/favicon-32.png"
+  },
+  {
+    "revision": "5e52768b0f7cbb62cbe07751add33101",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "2fdffc375b6944ee26735afd8e93c206",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90a14aeaf3ada9c8ed2948e1742084b0",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "4197a9fd61e635cea7da7906870ef364",
+    "url": "/register-service-worker.js"
+  }
+];
